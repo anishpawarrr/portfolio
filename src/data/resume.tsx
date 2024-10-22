2d36e505d04eb59fd912c5189ca2fc2184aa7f15
anishpawarrr <106865028+anishpawarrr@users.noreply.github.com>
2024-10-22 14:47:03 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,15 +2,15 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
+  name: "Anish Pawar",
+  initials: "AP",
   url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  location: "Pune, India",
+  locationLink: "https://maps.app.goo.gl/73z75tWEFjxNuDav7,
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    "",
   avatarUrl: "/me.png",
   skills: [
     "React",
